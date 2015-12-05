83a8081352e09b1c52f2025142348526c87611c8
Ethan Chen <intervigil@gmail.com>
2015-12-05 15:26:41 -0800

    bluetooth: Increase bluesleep TX interval
    Change-Id: I078ffc4084890adbaec0c392d4c48b9631345e44

--- drivers/bluetooth/bluesleep.c
+++ drivers/bluetooth/bluesleep.c
@@ -114,8 +114,8 @@ DECLARE_DELAYED_WORK(sleep_workqueue, bluesleep_sleep_work);
 #define bluesleep_tx_idle()     schedule_delayed_work(&sleep_workqueue, 0)
 #define bluesleep_hsuart_clk_check()     schedule_delayed_work(&sleep_workqueue, msecs_to_jiffies(10))
 
-/* 1 second timeout */
-#define TX_TIMER_INTERVAL  1 //HTC_BT: change from 5 to 1
+/* 5 second timeout */
+#define TX_TIMER_INTERVAL  5
 
 /* state variable names and bit positions */
 #define BT_PROTO		0x01    // Set / unset when start / stop Sleep-Mode Protocol
